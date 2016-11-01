c4d64775e6ce70e0e27aaa2c6cfd2cdd47b1d850
Dennis Gijsbers <dennis2727@hotmail.com>
2016-11-01 23:28:37 +0100

    teamApp verandert naar team Start gemaakt met mongoose in de api.

--- Applicatie/Server/app.js
+++ Applicatie/Server/app.js
@@ -7,12 +7,12 @@ var app = express();
 var quizmaster = require('./Routes/quizmaster');
 var scorebord = require('./Routes/scorebord');
 var teamApp = require('./Routes/teamApp');
-var quiz = require('./Routes/quiz')
+var quiz = require('./Routes/quiz');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/quizmaster', quizmaster);
 app.use('/scorebord', scorebord);
 app.use('/teamApp', teamApp);
-app.use('/quiz', quiz)
+app.use('/quiz', quiz);
 app.listen(3000);
