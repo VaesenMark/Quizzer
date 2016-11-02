b610f4cc3860f2ae004a6e805057d0f58738d011
Mark Vaesen <Markvaesen@gmail.com>
2016-11-02 12:24:28 +0100

    teamApp naar team (route)

--- Applicatie/Server/app.js
+++ Applicatie/Server/app.js
@@ -6,13 +6,13 @@ var app = express();
 
 var quizmaster = require('./Routes/quizmaster');
 var scorebord = require('./Routes/scorebord');
-var teamApp = require('./Routes/teamApp');
+var team = require('./Routes/team');
 var quiz = require('./Routes/quiz');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/quizmaster', quizmaster);
 app.use('/scorebord', scorebord);
-app.use('/teamApp', teamApp);
+app.use('/team', team);
 app.use('/quiz', quiz);
 app.listen(3000);
